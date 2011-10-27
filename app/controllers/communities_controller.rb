7926cb9904b4ba3b8795faf28a9bfeb5d13607dd
Cameron Cundiff <ckundo@gmail.com>
2011-10-27 12:24:35 -0400

    fixing countdown

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -2,6 +2,7 @@ class CommunitiesController < ApplicationController
 
   def index
     @communities = Community.all
+    @next_event = Event.all.sort {|a,b| a.start_time <=> b.start_time}.last
   end
 
   def show
