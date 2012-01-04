14036ef69fe0584f6b643d36b3ec122e9bc82e0f
Cameron Cundiff <ckundo@gmail.com>
2012-01-03 21:43:21 -0500

    adding asset compile flag for deploying to heroku

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module OccupyRails
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    called config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
